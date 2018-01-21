f4dc344fbc66fd8c11f0f93c8a7f720fa31bbd9a
Praveen Pillai <praveen.pillai@landmark.co.uk>
2018-01-21 15:30:21 +0530

    Out Variable deom in c# 7

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace c_7outvariabledemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //old way of using out variable
+            DateTime dt1;
+            if(DateTime.TryParse("12/12/2014",out dt1))
+            Console.WriteLine("Date Time Parsed in old way is {0}",dt1);
+
+            //New way of using out variable in c# 7
+
+            if(DateTime.TryParse("12/12/2014",out var dt2))
+            Console.WriteLine("Date Time parsed in new way is {0}",dt2);
+
+            Console.ReadLine();
         }
     }
 }
